8d89e7dd75c63bb8e31fdb1a35727950762d7bc3
Zhuang <zhuang17.han@gmail.com>
2018-10-17 22:48:49 +0800

    add global style js

---
+++ src/styles.js
@@ -0,0 +1,25 @@
+import Typography from '@material-ui/core/Typography';
+import {grey} from '@material-ui/core/colors';
+
+const styles = {
+  navigation: {
+    fontSize: 15,
+    fontWeight: Typography.fontWeightLight,
+    color: grey[600],
+    paddingBottom: 15,
+    display: 'block'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: Typography.fontWeightLight,
+    marginBottom: 20
+  },
+  paper: {
+    padding: 30
+  },
+  clear: {
+    clear: 'both'
+  }
+};
+
+export default styles;
