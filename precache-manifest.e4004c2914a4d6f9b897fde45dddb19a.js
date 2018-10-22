c44a6a7d0eaeefbc39555d9abc7375d55e113f37
Zhuang <zhuang17.han@gmail.com>
2018-10-22 17:33:17 +0800

    Updates

---
+++ precache-manifest.e4004c2914a4d6f9b897fde45dddb19a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-material-ui-admin-template/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "d43a28216b4d76f5bec7",
+    "url": "/react-material-ui-admin-template/static/css/main.23b08e41.chunk.css"
+  },
+  {
+    "revision": "011201ab56695ce86ea2f190bce2670b",
+    "url": "/react-material-ui-admin-template/static/media/favicon.011201ab.ico"
+  },
+  {
+    "revision": "a18a5ab5e111f1da2c1c",
+    "url": "/react-material-ui-admin-template/static/js/1.a18a5ab5.chunk.js"
+  },
+  {
+    "revision": "bfb0654117847e6fc976",
+    "url": "/react-material-ui-admin-template/static/js/runtime~main.bfb06541.js"
+  },
+  {
+    "revision": "abe4a318dfd1daed6d59f969027b0b53",
+    "url": "/react-material-ui-admin-template/static/media/material_bg.abe4a318.png"
+  },
+  {
+    "revision": "d43a28216b4d76f5bec7",
+    "url": "/react-material-ui-admin-template/static/js/main.d43a2821.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-material-ui-admin-template/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-material-ui-admin-template/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-material-ui-admin-template/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-material-ui-admin-template/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a18a5ab5e111f1da2c1c",
+    "url": "/react-material-ui-admin-template/static/css/1.11e6f8bf.chunk.css"
+  },
+  {
+    "revision": "c4bb3f80aaf2e6f37a4a07b633d35264",
+    "url": "/react-material-ui-admin-template/index.html"
+  }
+];
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { render } from "react-dom";
-import routes from "./routes";
-import registerServiceWorker from "./registerServiceWorker";
-import "./styles.scss";
-import "font-awesome/css/font-awesome.css";
-require("./favicon.ico");
-
-render(routes, document.getElementById("root"));
-registerServiceWorker();
