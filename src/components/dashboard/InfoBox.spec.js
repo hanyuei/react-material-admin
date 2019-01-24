c2b398e4def0391bc4c58ad838f49114b1c43ad6
Zhuang <zhuang17.han@gmail.com>
2019-01-24 20:30:57 +0800

    add ci

--- src/components/dashboard/InfoBox.spec.js
+++ src/components/dashboard/InfoBox.spec.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
 import { shallow } from "enzyme";
 import { expect } from "chai";
 import { configure } from "enzyme";
@@ -8,10 +9,11 @@ configure({ adapter: new Adapter() });
 
 describe("<InfoBox />", () => {
   it("should have a span with the title and value", () => {
-    const wrapper = shallow(<InfoBox title="Title" value="1500" />);
+    const wrapper = shallow(
+      <InfoBox title="Title" value="1500" color="red" Icon={ShoppingCart} />
+    );
     const content = wrapper.find("div > span");
 
-    // expect(content).to.have.length.of(2);
     expect(content).to.have.length(2);
     expect("Title").to.equal(content.at(0).text());
     expect("1500").to.equal(content.at(1).text());
