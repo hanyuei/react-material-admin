eea1514cdf30474d8d137f06e13f3d965ce006c9
Dali <dali110.han@gmail.com>
2019-10-15 22:59:03 +0800

    Updates

---
+++ precache-manifest.ec2ebe6bc383ed545a038eddb6cba31c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-material-admin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "d128579a2a83399df673",
+    "url": "/react-material-admin/static/css/main.da12623f.chunk.css"
+  },
+  {
+    "revision": "f720ee1dec0f84303a39",
+    "url": "/react-material-admin/static/js/runtime~main.cad08fbd.js"
+  },
+  {
+    "revision": "011201ab56695ce86ea2f190bce2670b",
+    "url": "/react-material-admin/static/media/favicon.011201ab.ico"
+  },
+  {
+    "revision": "ac4406cc07d8732a22b6",
+    "url": "/react-material-admin/static/js/2.4a61fa5c.chunk.js"
+  },
+  {
+    "revision": "abe4a318dfd1daed6d59f969027b0b53",
+    "url": "/react-material-admin/static/media/material_bg.abe4a318.png"
+  },
+  {
+    "revision": "d128579a2a83399df673",
+    "url": "/react-material-admin/static/js/main.796c4092.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-material-admin/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-material-admin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-material-admin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-material-admin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ac4406cc07d8732a22b6",
+    "url": "/react-material-admin/static/css/2.47131326.chunk.css"
+  },
+  {
+    "revision": "cf861ec958b5ea293557ee7fbb9bff35",
+    "url": "/react-material-admin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-material-admin/precache-manifest.2b777365c0b9496b0f72ef0ee6de0344.js"
+  "/react-material-admin/precache-manifest.ec2ebe6bc383ed545a038eddb6cba31c.js"
 );
 
 workbox.clientsClaim();
