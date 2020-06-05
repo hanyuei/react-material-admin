e018868ec33f22f016a01526100b6577852983a8
Dali <dali110.han@gmail.com>
2020-06-05 22:56:09 +0800

    update homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,67 +1,67 @@
-import React from "react";
-import PropTypes from "prop-types";
-import classNames from "classnames";
-import AppBar from "@material-ui/core/AppBar";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import InputBase from "@material-ui/core/InputBase";
-import SearchIcon from "@material-ui/icons/Search";
-import { GithubCircle } from "mdi-material-ui";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import MailIcon from "@material-ui/icons/Mail";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import MoreIcon from "@material-ui/icons/MoreVert";
-import Badge from "@material-ui/core/Badge";
-import { Toolbar } from "@material-ui/core";
-import { fade } from "@material-ui/core/styles/colorManipulator";
-import { withStyles } from "@material-ui/core/styles";
+import React from "react"
+import PropTypes from "prop-types"
+import classNames from "classnames"
+import AppBar from "@material-ui/core/AppBar"
+import IconButton from "@material-ui/core/IconButton"
+import MenuIcon from "@material-ui/icons/Menu"
+import InputBase from "@material-ui/core/InputBase"
+import SearchIcon from "@material-ui/icons/Search"
+import { GithubCircle } from "mdi-material-ui"
+import AccountCircle from "@material-ui/icons/AccountCircle"
+import MailIcon from "@material-ui/icons/Mail"
+import NotificationsIcon from "@material-ui/icons/Notifications"
+import MoreIcon from "@material-ui/icons/MoreVert"
+import Badge from "@material-ui/core/Badge"
+import { Toolbar } from "@material-ui/core"
+import { fade } from "@material-ui/core/styles/colorManipulator"
+import { withStyles } from "@material-ui/core/styles"
 
-const styles = theme => ({
+const styles = (theme) => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
+      duration: theme.transitions.duration.leavingScreen,
+    }),
   },
   appBarShift: {
     // marginLeft: theme.drawer.width,
     width: `calc(100% - ${theme.drawer.width}px)`,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   root: {
-    width: "100%"
+    width: "100%",
   },
   grow: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   menuButton: {
     marginLeft: -12,
-    marginRight: 20
+    marginRight: 20,
   },
   title: {
     display: "none",
     [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
+      display: "block",
+    },
   },
   search: {
     position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
     "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing.unit * 2,
     marginLeft: 0,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing.unit * 3,
-      width: "auto"
-    }
+      width: "auto",
+    },
   },
   searchIcon: {
     width: theme.spacing.unit * 9,
@@ -70,11 +70,11 @@ const styles = theme => ({
     pointerEvents: "none",
     display: "flex",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   inputRoot: {
     color: "inherit",
-    width: "100%"
+    width: "100%",
   },
   inputInput: {
     paddingTop: theme.spacing.unit,
@@ -84,55 +84,55 @@ const styles = theme => ({
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
-      width: 200
-    }
+      width: 200,
+    },
   },
   sectionDesktop: {
     display: "none",
     [theme.breakpoints.up("md")]: {
-      display: "flex"
-    }
+      display: "flex",
+    },
   },
   sectionMobile: {
     display: "flex",
     [theme.breakpoints.up("md")]: {
-      display: "none"
-    }
-  }
-});
+      display: "none",
+    },
+  },
+})
 
 class Header extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       anchorEl: null,
-      mobileMoreAnchorEl: null
-    };
+      mobileMoreAnchorEl: null,
+    }
   }
 
-  handleProfileMenuOpen = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
+  handleProfileMenuOpen = (event) => {
+    this.setState({ anchorEl: event.currentTarget })
+  }
 
   handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-    this.handleMobileMenuClose();
-  };
+    this.setState({ anchorEl: null })
+    this.handleMobileMenuClose()
+  }
 
-  handleMobileMenuOpen = event => {
-    this.setState({ mobileMoreAnchorEl: event.currentTarget });
-  };
+  handleMobileMenuOpen = (event) => {
+    this.setState({ mobileMoreAnchorEl: event.currentTarget })
+  }
 
   handleMobileMenuClose = () => {
-    this.setState({ mobileMoreAnchorEl: null });
-  };
+    this.setState({ mobileMoreAnchorEl: null })
+  }
 
   render() {
-    const { handleChangeNavDrawer, classes, navDrawerOpen } = this.props;
+    const { handleChangeNavDrawer, classes, navDrawerOpen } = this.props
 
-    const { anchorEl } = this.state;
-    const isMenuOpen = Boolean(anchorEl);
+    const { anchorEl } = this.state
+    const isMenuOpen = Boolean(anchorEl)
     // const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
     // const renderMenu = (
@@ -193,7 +193,7 @@ class Header extends React.Component {
       <div>
         <AppBar
           className={classNames(classes.appBar, {
-            [classes.appBarShift]: navDrawerOpen
+            [classes.appBarShift]: navDrawerOpen,
           })}
         >
           <Toolbar>
@@ -213,7 +213,7 @@ class Header extends React.Component {
                 placeholder="Search…"
                 classes={{
                   root: classes.inputRoot,
-                  input: classes.inputInput
+                  input: classes.inputInput,
                 }}
               />
             </div>
@@ -250,7 +250,7 @@ class Header extends React.Component {
                 aria-haspopup="true"
                 onClick={() =>
                   window.open(
-                    "https://github.com/dali110/react-material-admin"
+                    "https://github.com/dashutadie/react-material-admin"
                   )
                 }
                 color="inherit"
@@ -270,7 +270,7 @@ class Header extends React.Component {
           </Toolbar>
         </AppBar>
       </div>
-    );
+    )
   }
 }
 
@@ -278,7 +278,7 @@ Header.propTypes = {
   styles: PropTypes.object,
   handleChangeNavDrawer: PropTypes.func,
   classes: PropTypes.object,
-  navDrawerOpen: PropTypes.bool
-};
+  navDrawerOpen: PropTypes.bool,
+}
 
-export default withStyles(styles)(Header);
+export default withStyles(styles)(Header)
