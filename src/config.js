51a91f3a12899ede57726d5e8e42d2686bbcb811
Mark Murphy <mobitinker@gmail.com>
2020-10-30 14:03:17 -0700

    Port change

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ module.exports= {
 
   NODE_ENV: process.env.NODE_ENV || 'development',
   HOST: process.env.HOST || '127.0.0.1',
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 8080, //3000,
   /*
   DB_ENDPOINT: process.env.DB_ENDPOINT,
   DB_KEY: process.env.DB_KEY,
